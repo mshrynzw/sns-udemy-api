fee5c62d30ad85fcdcf9fbe4deda9b7ac8b7ecfb
kaoruyuki <>
2024-01-17 03:57:52 +0900

    Revert "16. トークンを発行するためにjsonwebtokenを利用してみよう"
    This reverts commit 2fd184ec851088bf9c1c5121b2dc547e904c2c99.

--- api/server.js
+++ api/server.js
@@ -26,21 +26,21 @@ app.post("/api/auth/register", async (req, res) => {
 });
 
 //ユーザーログインAPI
-app.post("/api/auth/login", async (req, res) => {
-  const { email, password } = req.body;
+app.post("/api/auth/register", async (req, res) => {
+  const { username, email, password } = req.body;
 
   const user = await prisma.user.findUnique({ where: { email } });
   if (!user) {
     return res.status(401).json({ error: "そのユーザーは存在しません。" });
   }
 
-  const isPasswordValid = await bcrypt.compare(password, user.password);
-  if (!isPasswordValid) {
+  const isPasswordValid = await bcrypt.compare(password, user.passwerd);
+  if (isPasswordValid) {
     return res.status(401).json({ error: "そのパスワードは間違っています。" });
   }
 
   const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY, {
-    expiresIn: "1d",
+    expriresIn: "1d",
   });
 
   return res.json({ token });
