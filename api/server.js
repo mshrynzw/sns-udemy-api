6fc6ec6d0722c924892be9f5bb56a5094f19cfe0
kaoruyuki <>
2024-01-17 03:20:21 +0900

    14. Thunder Clientを使ってAPIテストをやってみよう

--- api/server.js
+++ api/server.js
@@ -4,16 +4,16 @@ const app = express();
 const bcrypt = require("bcrypt");
 
 const PORT = 8000;
-
 const prisma = new PrismaClient();
 
+app.use(express.json());
 // 新規ユーザー登録API
 app.post("/api/auth/register", async (req, res) => {
   const { username, email, password } = req.body;
   const hashedPassword = await bcrypt.hash(password, 10);
 
   const user = await prisma.user.create({
-    dataa: {
+    data: {
       username,
       email,
       password: hashedPassword,
