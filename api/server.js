c4a1445c6db9135d85f1a89a4f36c2d348d318be
kaoruyuki <>
2024-01-17 09:53:02 +0900

    18. API処理を見やすい形にリファクタリングしておこう

--- api/server.js
+++ api/server.js
@@ -1,49 +1,13 @@
-const { PrismaClient } = require("@prisma/client");
 const express = require("express");
 const app = express();
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+const authRoute = require("./routers/auth");
+
 require("dotenv").config();
 
 const PORT = 8000;
-const prisma = new PrismaClient();
 
 app.use(express.json());
-// 新規ユーザー登録API
-app.post("/api/auth/register", async (req, res) => {
-  const { username, email, password } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
 
-  const user = await prisma.user.create({
-    data: {
-      username,
-      email,
-      password: hashedPassword,
-    },
-  });
-
-  return res.json({ user });
-});
-
-//ユーザーログインAPI
-app.post("/api/auth/login", async (req, res) => {
-  const { email, password } = req.body;
-
-  const user = await prisma.user.findUnique({ where: { email } });
-  if (!user) {
-    return res.status(401).json({ error: "そのユーザーは存在しません。" });
-  }
-
-  const isPasswordValid = await bcrypt.compare(password, user.password);
-  if (!isPasswordValid) {
-    return res.status(401).json({ error: "そのパスワードは間違っています。" });
-  }
-
-  const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY, {
-    expiresIn: "1d",
-  });
-
-  return res.json({ token });
-});
+app.use("/api/auth", authRoute);
 
 app.listen(PORT, () => console.log(`server is runing on Port ${PORT}`));
