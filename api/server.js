1a2bbde5626633e8b652957e4b2aa528ffa13eca
kaoruyuki <>
2024-01-20 18:05:35 +0900

    25. CORSエラーを解決してみよう

--- api/server.js
+++ api/server.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
 const authRoute = require("./routers/auth");
+const cors = require("cors");
 
 require("dotenv").config();
 
 const PORT = 8000;
 
+app.use(cors());
 app.use(express.json());
-
 app.use("/api/auth", authRoute);
 
 app.listen(PORT, () => console.log(`server is runing on Port ${PORT}`));
